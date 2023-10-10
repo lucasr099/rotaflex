164073bf89200deba6c1460f7d7c3640d5073437
lucasr099 <141447167+lucasr099@users.noreply.github.com>
2023-10-09 23:06:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calcularDestino() {
-    const origemInput = document.getElementById("origem").value.toLowerCase(); // Converta para minúsculas
-    const destinoInput = document.getElementById("destino").value.toLowerCase(); // Converta para minúsculas
+    const origemInput = document.getElementById("origem").value.toLowerCase(); 
+    const destinoInput = document.getElementById("destino").value.toLowerCase(); 
 
-    // Verifique se a origem é "caixa d'água" e o destino é "paralela"
+    
     if (origemInput === "caixa d'água" && destinoInput === "paralela") {
-        // Se sim, defina a mensagem personalizada e exiba a imagem
-        const mensagem = `A melhor rota de ${origemInput} a ${destinoInput} é via Av. Luís Viana. Demorará apenas 16 minutos de carro e motocicleta, e a distância é de 13,1 km.`;
+      
+        const mensagem = `A rota mais eficiente ${origemInput} à ${destinoInput} é via Av. Luís Viana Filho. O percurso de carro ou motocicleta levará apenas 16 minutos, cobrindo uma distância de 13,1 km.`;
         const resultadoDiv = document.getElementById("resultado");
         resultadoDiv.innerHTML = `<p>${mensagem}</p><img src="imagens/img.png" alt="Imagem do destino">`;
     } else {
